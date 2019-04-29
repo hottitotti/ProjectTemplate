c32f5027c7782b68d2a46576e6c50070c8d4e92a
Aidan Noble-Goodman <anoblegoodman@Aidans-MacBook-Pro.local>
2019-04-29 14:47:27 -0700

    cleanup of ActionTypes.jsx, no functional changes

--- client/actions/ActionTypes.jsx
+++ client/actions/ActionTypes.jsx
@@ -1,6 +1,3 @@
-
-
-
 export const CREATE_USER = 'CREATE_USER';
 export const CREATE_CARD = 'CREATE_TICKET';
 export const DELETE_CARD = 'DELETE_TICKET';
@@ -10,8 +7,3 @@ export const FILTERBY_ALL = 'FILTERBY_ALL';
 export const FILTERBY_STATUS = 'FILTERBY_STATUS';
 export const FILTERBY_USER = 'FILTERBY_USER';
 export const FILTERBY_DATE = 'FILTERBY_DATE';
-
-export const onUpdateText = (value) => ({
-  type: types.UPDATE_TEXT,
-  payload: value,
-})
